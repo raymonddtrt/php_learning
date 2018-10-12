f91bf188d7cbe1137610d0db21e8e4e59492dd06
raymonddtrt <42290259+raymonddtrt@users.noreply.github.com>
2018-10-12 11:33:25 +0800

    Create BMI1.php

---
+++ 20181012_if/BMI1.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+ </head>
+ <body>
+  <form method="post" action="BMI2.php">
+  	<table>
+	<tr>
+		<td>性別</td>
+		<td>
+			<input type="radio" name="gender" value="male">男生
+			<input type="radio" name="gender" value="female">女生
+		</td>
+	</tr>
+	<tr>
+		<td>體重</td>
+		<td>
+			<input type="text" name="weight">
+		</td>
+	</tr>
+	<tr>
+		<td>身高</td>
+		<td>
+			<input type="text" name="height">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<input type="submit">
+		</td>
+		<td></td>
+	</tr>
+	</table>
+  
+  </form>
+ </body>
+</html>
