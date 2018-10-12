e920b354f4d3f60879cecabd640ca6368e7bb844
raymonddtrt <42290259+raymonddtrt@users.noreply.github.com>
2018-10-12 09:04:29 +0800

    Create score1.php

---
+++ 20181005/score1.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+ </head>
+ <body>
+	<form method="post" action="score2.php">
+		<table border=5 align=center>
+		<tr>
+			<td>國文</td>
+			<td>
+				<input type="text" name="chineseScore">
+			</td>
+		</tr>
+		<tr>
+			<td>英文</td>
+			<td>
+				<input type="text" name="englishScore">
+			</td>
+		</tr>
+		<tr>
+			<td>數學</td>
+			<td>
+				<input type="text" name="mathScore">
+			</td>
+		</tr>
+		<tr align=center>
+			<td colspan=2>
+				<input type="submit" name="送出"></input>
+			</td>
+		</tr>
+		</table>
+		
+		
+	
+	
+	</form>
+ </body>
+</html>
