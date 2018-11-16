89492f1c3520d4fe294331152b2afa5694bf1729
raymonddtrt <42290259+raymonddtrt@users.noreply.github.com>
2018-11-16 12:03:13 +0800

    Create test1.php

---
+++ 20181116/test1.php
@@ -0,0 +1,183 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+  <script type="text/javascript" src="http://www.pureexample.com/js/lib/jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="http://www.pureexample.com/js/flot/jquery.flot.min.js"></script>
+<style type="text/css">
+        #flotid {width:50%;height:300px;}        
+ </style>
+
+ </head>
+ <body>
+
+<form method="post" action="test2.php">
+<table>
+<tr>
+<td><label>風險股</label></td>
+</tr>
+<tr>
+	<td>1月</td>
+	<td>
+		<input type="text" name="s1">
+	</td>
+</tr>
+<tr>
+	<td>2月</td>
+	<td>
+		<input type="text" name="s2">
+	</td>
+</tr>
+<tr>
+	<td>3月</td>
+	<td>
+		<input type="text" name="s3">
+	</td>
+</tr>
+<tr>
+	<td>4月</td>
+	<td>
+		<input type="text" name="s4">
+	</td>
+</tr>
+<tr>
+	<td>5月</td>
+	<td>
+		<input type="text" name="s5">
+	</td>
+</tr>
+<tr>
+	<td>6月</td>
+	<td>
+		<input type="text" name="s6">
+	</td>
+</tr>
+<tr>
+	<td>7月</td>
+	<td>
+		<input type="text" name="s7">
+	</td>
+</tr>
+<tr>
+	<td>8月</td>
+	<td>
+		<input type="text" name="s8">
+	</td>
+</tr>
+<tr>
+	<td>9月</td>
+	<td>
+		<input type="text" name="s9">
+	</td>
+</tr>
+<tr>
+	<td>10月</td>
+	<td>
+		<input type="text" name="s10">
+	</td>
+</tr>
+<tr>
+	<td>11月</td>
+	<td>
+		<input type="text" name="s11">
+	</td>
+</tr>
+<tr>
+	<td>12月</td>
+	<td>
+		<input type="text" name="s12">
+	</td>
+	
+</tr>
+
+<tr>
+	<td><label>績優股</label></td>
+</tr>
+
+<tr>
+
+	<td>1月</td>
+	<td>
+		<input type="text" name="k1">
+	</td>
+</tr>
+<tr>
+	<td>2月</td>
+	<td>
+		<input type="text" name="k2">
+	</td>
+</tr>
+<tr>
+	<td>3月</td>
+	<td>
+		<input type="text" name="k3">
+	</td>
+</tr>
+<tr>
+	<td>4月</td>
+	<td>
+		<input type="text" name="k4">
+	</td>
+</tr>
+<tr>
+	<td>5月</td>
+	<td>
+		<input type="text" name="k5">
+	</td>
+</tr>
+<tr>
+	<td>6月</td>
+	<td>
+		<input type="text" name="k6">
+	</td>
+</tr>
+<tr>
+	<td>7月</td>
+	<td>
+		<input type="text" name="k7">
+	</td>
+</tr>
+<tr>
+	<td>8月</td>
+	<td>
+		<input type="text" name="k8">
+	</td>
+</tr>
+<tr>
+	<td>9月</td>
+	<td>
+		<input type="text" name="k9">
+	</td>
+</tr>
+<tr>
+	<td>10月</td>
+	<td>
+		<input type="text" name="k10">
+	</td>
+</tr>
+<tr>
+	<td>11月</td>
+	<td>
+		<input type="text" name="k11">
+	</td>
+</tr>
+<tr>
+	<td>12月</td>
+	<td>
+		<input type="text" name="k12">
+	</td>
+</tr>
+
+<td><input type="submit" value="提交"></td>
+</table>
+</form>
+
+ </body>
+</html>
+
+
