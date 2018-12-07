66732c531b670b8df0aba0bdc4369fe3714ca451
raymonddtrt <42290259+raymonddtrt@users.noreply.github.com>
2018-12-07 11:54:37 +0800

    Create odbc1.php

---
+++ 20181207/odbc1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+ </head>
+ <form method=post action="odbc2.php">
+
+	department:<input type=text name="input1">
+	school:<input type=text name="input2">
+	<input type="submit">
+ </form>
+ <body>
+
+
+ </body>
+</html>
