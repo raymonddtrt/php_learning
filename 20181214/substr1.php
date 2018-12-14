e629c778c3ac48218f07e188c7c28a9a74242a3f
raymonddtrt <42290259+raymonddtrt@users.noreply.github.com>
2018-12-14 08:56:28 +0800

    Create substr1.php

---
+++ 20181214/substr1.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+ </head>
+ <body>
+  <form method="post" action="substr2.php">
+  <table>
+  <tr>
+	<td>
+		<input type="text" name="inputData"></input>
+	</td>
+  </tr>
+  <tr>
+	<td>
+		<input type="submit"></input>
+	</td>
+  </tr>
+  </table>
+  
+  
+  </form>
+ </body>
+</html>
